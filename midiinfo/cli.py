6db6cec3ba7bf4d77b54037c9bc03ee0a5da640b
8gaU8 <haga.yuya2.2016@gmail.com>
2024-02-05 22:31:40 +0900

    pygame とrtmidiどっちにも対応

--- midiinfo/cli.py
+++ midiinfo/cli.py
@@ -1,22 +1,53 @@
-from typing import Union
+from typing import Literal, Union
 
-from rtmidi import MidiIn, MidiOut  # type: ignore
-
-
-def _print_ports_names(midi_port: Union[MidiIn, MidiOut]):
-    for i, port in enumerate(midi_port.get_ports()):
-        print(f"\t{i}: {port}")
+from fire import Fire
 
 
-def print_midi_info():
+def print_midi_info_pygame():
+    print("using backend: pygame")
+    import pygame.midi as m
+
+    m.init()
+    dev_num = m.get_count()
+    outputs = []
+    inputs = []
+    for dev_id in range(dev_num):
+        (interf, name, is_input, is_output, _) = m.get_device_info(dev_id)
+        msg = f"\n\t{dev_id}: {name.decode()} [{interf.decode()}]"
+        if is_input == 1:
+            inputs.append(msg)
+        elif is_output == 1:
+            outputs.append(msg)
+    print(" Inputs: ", *inputs)
+    print("Outputs: ", *outputs)
+    m.quit()
+
+
+def print_midi_info_rtmidi():
+    print("using backend: rtmidi")
+    from rtmidi import MidiIn, MidiOut  # type: ignore
+
+    def print_ports_names(midi_port: Union[MidiIn, MidiOut]):
+        for i, port in enumerate(midi_port.get_ports()):
+            print(f"\t{i}: {port}")
+
     midiout = MidiOut()
     midiin = MidiIn()
 
     print("Outputs: ")
-    _print_ports_names(midiout)
-    print("inputs: ")
-    _print_ports_names(midiin)
+    print_ports_names(midiout)
+    print("Inputs: ")
+    print_ports_names(midiin)
+
+
+def print_midi_info(backend: Literal["pygame", "rtmidi"] = "pygame"):
+    if backend == "pygame":
+        print_midi_info_pygame()
+        return
+    if backend == "rtmidi":
+        print_midi_info_rtmidi()
+        return
 
 
 def main_cli():
-    print_midi_info()
+    Fire(print_midi_info)
