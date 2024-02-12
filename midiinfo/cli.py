17aa2414fc6b61e1d5aac7ec3aa0cd756b29f582
8gaU8 <haga.yuya2.2016@gmail.com>
2024-02-12 21:58:51 +0900

    デバイス名を二重引用符で囲った

--- midiinfo/cli.py
+++ midiinfo/cli.py
@@ -13,7 +13,7 @@ def print_midi_info_pygame():
     inputs = []
     for dev_id in range(dev_num):
         (interf, name, is_input, is_output, _) = m.get_device_info(dev_id)
-        msg = f"\n\t{dev_id}: {name.decode()} [{interf.decode()}]"
+        msg = f'\n\t{dev_id}: "{name.decode()}" [{interf.decode()}]'
         if is_input == 1:
             inputs.append(msg)
         elif is_output == 1:
@@ -29,7 +29,7 @@ def print_midi_info_rtmidi():
 
     def print_ports_names(midi_port: Union[MidiIn, MidiOut]):
         for i, port in enumerate(midi_port.get_ports()):
-            print(f"\t{i}: {port}")
+            print(f'\t{i}: "{port}"')
 
     midiout = MidiOut()
     midiin = MidiIn()
